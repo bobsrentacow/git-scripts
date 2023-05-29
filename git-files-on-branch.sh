4f2fb6d75f430c11318e8366c9c3117d2e8531dc
Matt Mahin <matt.mahin@qsc.com>
2023-05-28 20:53:44 -0600

    finds branches containing specific files (with sed regex)

---
+++ git-files-on-branch.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/bash
+
+if [[ "$#" -ne 1 ]] ; then
+  echo "usage: $0 <trunk_branch_name>"
+fi
+trunk="$1"
+
+echo "Displaying branches that have cache/* in their tree somewhere between the merge-base and HEAD"
+echo "of that branch.  Merge-base is checked on a per-branch basis relative to trunk."
+echo ""
+
+# find all branches, and merged branches, then create list of unmerged branches
+readarray -t branches <<< "$(git branch | sed 's/\(. \|  \)\(.*\)/\2/')"
+readarray -t merged <<< "$(git branch --merged "${trunk}" | sed 's/\(. \|  \)\(.*\)/\2/')"
+for del in "${merged[@]}"; do
+  for ii in "${!branches[@]}"; do
+    if [[ "${branches[ii]}" == "${del}" ]]; then
+      unset 'branches[ii]'
+    fi
+  done
+done
+
+for branch in "${branches[@]}" ; do
+  # merge_base=$(git merge-base "${trunk}" "${branch}")
+  # readarray -t commits <<< "$(git log --format=format:"%H" "${merge_base}".."${branch}")"
+  commits_text="$(git log --format=format:"%H" "$(git merge-base "${trunk}" "${branch}")".."${branch}")"
+  if [ -z "${commits_text}" ] ; then
+    continue
+  fi
+  readarray -t commits <<< "${commits_text}"
+
+  branch_printed=false
+  for commit in "${commits[@]}" ; do
+    tree_cache_text="$(git ls-tree -r "${commit}" | awk '$4 ~ /^cache\/\S+\//')"
+    if [ -z "${tree_cache_text}" ] ; then
+      continue
+    fi
+    if [ ${branch_printed} != true ]; then
+      echo "branch: ${branch}"
+      branch_printed=true
+      break
+    fi
+    echo "  commit: ${commit}"
+  done
+  if [ ${branch_printed} == true ]; then
+    continue
+  fi
+done
+
+# echo "${#branches[@]}"
+# for branch in "${branches[@]}" ; do
+#   echo "$branch"
+# #   if [ "${branch}" == "${merge_base}" ] ; then
+# #     continue
+# #   fi
+# #   echo "branch: ${branch}"
+# #   # merge_base=$(git merge-base "${trunk}" "${branch}")
+# #   # commits=($(git log --format=format:"%H" "${merge_base}".."${branch}"))
+
+# #   # for commit in "${commits[@]}" ; do
+# #   #   echo "commit: ${commit}"
+# #   #   # git ls-tree -r "${commit}" | awk '$4 ~ /^cache\//'  
+# #   #   git ls-tree -r e62587291358fe212bdccb72385ed1c50579ae56 | awk '$4 ~ /^cache\/\S+\//'
+# #   # done
+# done
